e0cfd42fc599f3d7d09611d2c521732682b92583
Kevin Krauss <earlkrauss@gmail.com>
2012-05-06 11:50:35 -0700

    Update mac-run.sh

--- mac-run.sh
+++ mac-run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-mongod --port 27047 --rest --dbpath=/Users/telmate/Documents/simpleKiosk/data/db/
+mongod --port 27047 --rest --dbpath=/Documents/simpleKiosk/data/db/
 ruby -rubygems simpleKioskController.rb
 /Developer/AdobeAirSDK2/bin/adl application.xml 
